693281bce2d9dbcadc822aeed0943184c6414b08
Treussart Matthieu <matthieu@treussart.com>
2019-01-26 21:22:50 +0000

    Update sendTempHumToDomoticz.py

--- cron/sendTempHumToDomoticz.py
+++ cron/sendTempHumToDomoticz.py
@@ -22,8 +22,8 @@ pin = args.pin
 
 def getTempHum(sensor, pin):
     sensor_args = {'11': Adafruit_DHT.DHT11,
-                    '22': Adafruit_DHT.DHT22,
-                    '2302': Adafruit_DHT.AM2302}
+                   '22': Adafruit_DHT.DHT22,
+                   '2302': Adafruit_DHT.AM2302}
     sensor = sensor_args[sensor]
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     return humidity, temperature
@@ -32,7 +32,8 @@ def getTempHum(sensor, pin):
 def sendToDomoticz(host, port, idx, temperature, humidity):
     if humidity is not None and temperature is not None:
         c = pycurl.Curl()
-        url = 'http://' + str(host) + ':' + str(port) + '/json.htm?type=command&param=udevice&idx=' + str(idx) + '&nvalue=0&svalue=' + str("{0:.2f}".format(temperature)) + ';' + str("{0:.2f}".format(humidity)) + ';0'
+        url = 'http://' + str(host) + ':' + str(port) + '/json.htm?type=command&param=udevice&idx=' + str(idx) + '&nvalue=0&svalue='\
+        + str("{0:.2f}".format(temperature)) + ';' + str("{0:.2f}".format(humidity)) + ';0'
         c.setopt(c.URL, url)
         c.perform()
         c.close()
